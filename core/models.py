2bba2d4a9e753ce677f71feb40bcc89ffeb41a1f
Mechantchulo <chulomechant@gmail.com>
2025-01-30 00:33:06 +0300

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete = models.CASCADE)
+    age  = models.IntegerField()
+    diagnosis = models.CharField(max_length = 100)
+    preferences = models.TextField()
+
+class BehaviorLog(models.Model):
+    user_profile = models.ForeignKey(UserProfile , on_delete = models.CASCADE)
+    behavior_description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    severity = models.CharField(max_length = 100)
+
+class Goal(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
+    goal_descritpion = models.TextField()
+    target_date = models.DateTimeField()
+    achieved = models.BooleanField(default = False)
+
+class Resource(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
