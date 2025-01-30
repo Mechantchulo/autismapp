0c8b9192119e75c1f7f091055a29fa65e3aec703
Mechantchulo <chulomechant@gmail.com>
2025-01-30 09:25:47 +0300

    Updated core/admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserProfile, BehaviorLog, Goal, Resource
+
+admin.site.register(UserProfile)
+admin.site.register(BehaviorLog)
+admin.site.register(Goal)
+admin.site.register(Resource)
